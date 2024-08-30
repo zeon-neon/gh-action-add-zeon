74908d512c28ab2da65aac34e52030b7d59a5a16
Saahil <neon@saahild.com>
2024-08-30 16:50:26 -0400

    feat(init): finish up

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-const core = require('@actions/core');
-const github = require('@actions/github');
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 try {
   core.setOutput("ok", true);
-    // Get the JSON webhook payload for the event that triggered the workflow
-    // still dev mode
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-console.log(`The event payload: ${payload}`);
-require('child_process').execSync(`git config --global user.email "zeon@saahild.com"`)
-require('child_process').execSync(`git config --global user.name "Zeon [ACTIONS]"`)
+  // Get the JSON webhook payload for the event that triggered the workflow
+  // still dev mode
+  const payload = JSON.stringify(github.context.payload, undefined, 2);
+  console.log(`The event payload: ${payload}`);
+  require("child_process").execSync(
+    `git config --global user.email "zeon@saahild.com"`
+  );
+  require("child_process").execSync(
+    `git config --global user.name "Zeon [ACTIONS]"`
+  );
 } catch (error) {
   core.setOutput("ok", false);
   core.setFailed(error.message);
